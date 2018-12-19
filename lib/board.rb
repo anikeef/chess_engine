0abaf5a795963399975daa02a5a1ee07ef754040
Gennadiy <genaydzhan70@gmail.com>
2018-12-19 23:57:36 +0600

    Add #set_default method

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,21 @@ class Board
       end
     end
   end
+
+  def set_default
+    [[:white, 1, 2], [:black, 8, 7]].each do |color, row1, row2|
+      @board["a#{row1}"] = Rook.new(color)
+      @board["h#{row1}"] = Rook.new(color)
+      @board["b#{row1}"] = Knight.new(color)
+      @board["g#{row1}"] = Knight.new(color)
+      @board["c#{row1}"] = Elephant.new(color)
+      @board["f#{row1}"] = Elephant.new(color)
+      @board["d#{row1}"] = Queen.new(color)
+      @board["e#{row1}"] = King.new(color)
+
+      ["a", "b", "c", "d", "e", "f", "g", "h"].each do |column|
+        @board["#{column}#{row2}"] = Pawn.new(color)
+      end
+    end
+  end
 end
