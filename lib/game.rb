6e088148738dc8ec8c28b4108a1c95706d72f629
Gennadiy <genaydzhan70@gmail.com>
2019-01-02 18:29:02 +0600

    Create Game#make_step method

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require "./lib/board.rb"
+require "./lib/player.rb"
 
 class Game
 
@@ -6,5 +7,19 @@ class Game
     @board = Board.new
     @board.set_default
     @players = [Player.new(:white), Player.new(:black)]
+    @players_cycle = @players.cycle
+    @current_player = @players_cycle.next
+  end
+
+  def make_step(from, to)
+    chesspiece = @board[from]
+    raise IncorrectInput, "Empty square is chosen" if chesspiece.nil?
+    raise IncorrectInput, "This is not your piece" unless chesspiece.color == @current_player.color
+
+    allowed_moves = chesspiece.get_allowed_moves(from)
+    raise IncorrectInput, "Illegal step" unless allowed_moves.include?(to)
+    raise IncorrectInput, "You can't beat your own piece" if !@board[to].nil? && @board[to].color == chesspiece.color
+
+    @board.move_piece(from, to)
   end
 end
---
+++ spec/game_spec.rb
@@ -0,0 +1,45 @@
+require "./lib/game.rb"
+
+describe Game do
+  before :each do
+    @game = Game.new
+  end
+
+  describe "#make_step" do
+    it "raises an error if the empty square is chosen" do
+      @game = Game.new
+      expect { @game.make_step("e3", "e4") }.to raise_error(IncorrectInput, "Empty square is chosen")
+    end
+
+    it "raises an error if a player choses not his own piece" do
+      @game = Game.new
+      expect { @game.make_step("a7", "a6") }.to raise_error(IncorrectInput, "This is not your piece")
+    end
+
+    it "raises an error if illegal step is made" do
+      @game = Game.new
+      chosen_piece = double("chosen_piece", :get_allowed_moves => ["e3", "e4"], :color => :white)
+      @game.instance_variable_set(:@board, {"e2" => chosen_piece})
+      expect { @game.make_step("e2", "e5") }.to raise_error(IncorrectInput, "Illegal step")
+    end
+
+    it "raises an error if the target square is taken by a piece with the same color" do
+      @game = Game.new
+      chosen_piece = double("chosen_piece", :color => :white, :get_allowed_moves => ["e3", "e4"])
+      target_piece = double("target_piece", :color => :white)
+      @game.instance_variable_set(:@board, {"e2" => chosen_piece, "e4" => target_piece})
+      expect { @game.make_step("e2", "e4") }.to raise_error(IncorrectInput, "You can't beat your own piece")
+    end
+
+    it "makes correct steps" do
+      @game = Game.new
+      chosen_piece = double("chosen_piece", :color => :white, :get_allowed_moves => ["e3", "e4"])
+      board = @game.instance_variable_get(:@board)
+      board["e2"] = chosen_piece
+      @game.instance_variable_set(:@board, board)
+      @game.make_step("e2", "e4")
+      expect(@game.instance_variable_get(:@board)["e4"]).to eq(chosen_piece)
+      expect(@game.instance_variable_get(:@board)["e2"]).to be_nil
+    end
+  end
+end
