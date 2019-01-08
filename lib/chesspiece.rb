db0d65ad2829158ae408bcb7995ec229b608485a
Gennadiy <genaydzhan70@gmail.com>
2019-01-08 17:30:51 +0600

    Create general #get_allowed_moves method

--- lib/chesspiece.rb
+++ lib/chesspiece.rb
@@ -1,8 +1,42 @@
+require "./lib/diagonals.rb"
+
 class ChessPiece
   attr_reader :symbol, :color
 
+  @@rows = ["8", "7", "6", "5", "4", "3", "2", "1"].map do |row|
+    ["a", "b", "c", "d", "e", "f", "g", "h"].map { |column| "#{column}#{row}" }
+  end
+  @@columns = @@rows.transpose
+  @@left_diagonals = @@rows.get_left_diagonals
+  @@right_diagonals = @@rows.get_right_diagonals
+
   def initialize(color)
     @color = color
+  end
+
+  def get_allowed_moves(position, current_board, areas)
+    allowed_moves = []
+
+    areas.each do |area|
+      line = area.find { |array| array.include?(position) }
+      index = line.find_index(position)
+
+      [line[0..(index - 1)].reverse, line[(index + 1)..-1]].each do |arr|
+        arr.each do |square_label|
+          square = current_board[square_label]
+
+          if square.nil?
+            allowed_moves << square_label
+          elsif square.color != self.color
+            allowed_moves << square_label
+            break
+          else
+            break
+          end
+        end
+      end
+    end
+    allowed_moves
   end
 end
 
@@ -18,6 +52,10 @@ class Rook < ChessPiece
     super
     @symbol = (@color == :black) ? "\u265C" : "\u2656"
   end
+
+  def get_allowed_moves(position, current_board)
+    super(position, current_board, [@@rows, @@columns])
+  end
 end
 
 class Knight < ChessPiece
@@ -32,6 +70,10 @@ class Elephant < ChessPiece
     super
     @symbol = (@color == :black) ? "\u25B2" : "\u25B3"
   end
+
+  def get_allowed_moves(position, current_board)
+    super(position, current_board, [@@left_diagonals, @@right_diagonals])
+  end
 end
 
 class King < ChessPiece
@@ -45,5 +87,9 @@ class Queen < ChessPiece
   def initialize(color)
     super
     @symbol = (@color == :black) ? "\u265B" : "\u2655"
+  end
+
+  def get_allowed_moves(position, current_board)
+    super(position, current_board, [@@rows, @@columns, @@left_diagonals, @@right_diagonals])
   end
 end
