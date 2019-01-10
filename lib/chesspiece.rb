4772f09fae7a7e311c1d6338097df597c00cd6d2
Gennadiy <genaydzhan70@gmail.com>
2019-01-10 17:35:39 +0600

    Create #allowed_moves method for Knight

--- lib/chesspiece.rb
+++ lib/chesspiece.rb
@@ -24,7 +24,7 @@ class ChessPiece
 
           if square.nil?
             allowed_moves << square_label
-          elsif square.color != self.color
+          elsif square.color != @color
             allowed_moves << square_label
             break
           else
@@ -60,6 +60,29 @@ class Knight < ChessPiece
   def initialize(color, board, position)
     super
     @symbol = (@color == :black) ? "\u265E" : "\u2658"
+  end
+
+  def allowed_moves
+    allowed_moves = []
+    row_index = 8 - @position[1].to_i
+    column_index = COLUMN_LETTERS.find_index(@position[0])
+
+    allowed_rows = [row_index + 2, row_index - 2, row_index + 1, row_index - 1].map do |index|
+      index >= 0 ? ROWS[index] : nil
+    end
+
+    allowed_rows[0..1].each do |row|
+      unless row.nil?
+        [column_index + 1, column_index - 1].each { |index| allowed_moves << row[index] if index.between?(0, 7) }
+      end
+    end
+
+    allowed_rows[2..3].each do |row|
+      unless row.nil?
+        [column_index + 2, column_index - 2].each { |index| allowed_moves << row[index] if index.between?(0, 7) }
+      end
+    end
+    allowed_moves.select { |label| @board[label].nil? || @board[label].color != @color }
   end
 end
 
