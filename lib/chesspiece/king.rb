228d98fdcfba074ee744d6e101530751ef9b642b
Gennadiy <genaydzhan70@gmail.com>
2019-01-13 11:52:42 +0600

    Refactor King#valid_moves for number access

--- lib/chesspiece/king.rb
+++ lib/chesspiece/king.rb
@@ -9,16 +9,11 @@ class King < ChessPiece
 
   def valid_moves
     valid_moves = []
-    horizontal_index = 8 - @position[1].to_i
-    vertical_index = COLUMN_LETTERS.find_index(@position[0])
-
     [0, 1, -1, 1, -1].permutation(2).to_a.uniq.each do |horizontal_step, vertical_step|
-      coordinates = [horizontal_index + horizontal_step, vertical_index + vertical_step]
-      if coordinates.all? { |c| c.between?(0, 7) }
-        label = ROWS[coordinates[0]][coordinates[1]]
-        square = @board[label]
-        valid_moves << label if square.nil? || square.color != @color
-      end
+      coordinates = [@position[0] + horizontal_step, @position[1] + vertical_step]
+      next unless @board.exists_at?(coordinates)
+      piece = @board.at(coordinates)
+      valid_moves << coordinates if piece.nil? || piece.color != @color
     end
     valid_moves
   end
