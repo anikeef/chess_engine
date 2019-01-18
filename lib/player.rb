0d42acbf11bebbfd2c28e6319dbb1f5ff00c0a8d
Gennadiy <genaydzhan70@gmail.com>
2019-01-18 17:03:17 +0600

    Shorten the Player#input_step method

--- lib/player.rb
+++ lib/player.rb
@@ -10,14 +10,10 @@ class Player
   end
 
   def input_step
-    column_letters = ["a"]
     print "#{@color.to_s.capitalize}'s move (e. g. \"e2 e4\"): "
     input = gets.gsub(/\s+/, "")
-    unless /^[abcdefgh][12345678][abcdefgh][12345678]$/.match?(input)
-      raise IncorrectInput, "Input must look like \"e2 e4\" or \"a6b5\""
-    end
-    from = [COLUMN_LETTERS.find_index(input[0]), input[1].to_i - 1]
-    to = [COLUMN_LETTERS.find_index(input[2]), input[3].to_i - 1]
-    [from, to]
+    raise IncorrectInput, "Input must look like \"e2 e4\" or \"a6b5\"" unless /^[a-h][1-8][a-h][1-8]$/.match?(input)
+    [[COLUMN_LETTERS.find_index(input[0]), input[1].to_i - 1],
+    [COLUMN_LETTERS.find_index(input[2]), input[3].to_i - 1]]
   end
 end
