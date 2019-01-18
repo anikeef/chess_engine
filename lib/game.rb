5c4d0429d5599772bdfe35b12b2b49d73ab314b0
Gennadiy <genaydzhan70@gmail.com>
2019-01-18 16:33:40 +0600

    Create Game#play and basic gameplay cycle

--- lib/game.rb
+++ lib/game.rb
@@ -21,4 +21,45 @@ class Game
 
     @board.move_piece(from, to)
   end
+
+  def play
+    until stalemate?
+      puts "\n"
+      puts @board
+      declare_check if check?
+      begin
+        make_step(*@current_player.input_step)
+      rescue IncorrectInput => e
+        puts "#{e.message}. Try again"
+        retry
+      end
+      @current_player = @players_cycle.next
+    end
+    game_over
+  end
+
+  def check?
+    @board.kings[@current_player.color].attacked?
+  end
+
+  def stalemate?
+    @board.pieces(@current_player.color).all? { |piece| piece.valid_moves.empty? }
+  end
+
+  def game_over
+    puts @board
+    if check?
+      declare_mate
+    else
+      puts "Stalemate!"
+    end
+  end
+
+  def declare_mate
+    puts "#{@current_player.color.to_s.capitalize} player got mated!"
+  end
+
+  def declare_check
+    puts "#{@current_player.color.to_s.capitalize} player is given a check"
+  end
 end
