20864af28feee19d1c9fe94d27093104403d5432
Gennadiy <genaydzhan70@gmail.com>
2019-01-19 16:18:38 +0600

    Create Pawn#en_passant_coordinates

--- lib/chesspiece/pawn.rb
+++ lib/chesspiece/pawn.rb
@@ -1,14 +1,16 @@
 require "./lib/chesspiece.rb"
 
 class Pawn < ChessPiece
+  attr_reader :direction
+
   def initialize(color, board, position)
     super
     @symbol = (@color == :black) ? "\u265F" : "\u2659"
+    @direction = (@color == :white) ? 1 : -1
   end
 
   def valid_moves
     valid_moves = []
-    direction = @color == :white ? 1 : -1
 
     coordinates = relative_coordinates([0, direction])
     next_piece = @board.at(coordinates)
@@ -18,5 +20,23 @@ class Pawn < ChessPiece
 
     valid_moves.push(*super([[-1, direction], [1, direction]]).reject { |coord| @board.at(coord).nil? })
     valid_moves.reject { |move| fatal_move?(move) }
+  end
+
+  def en_passant_coordinates(last_piece)
+    return unless @position[1].between?(3, 4) && last_piece.class == Pawn && last_piece.moves == 1
+    piece_coordinates = [relative_coordinates([1, 0]), relative_coordinates([-1, 0])].find do |coord|
+      @board.exists_at?(coord) && @board.at(coord) == last_piece
+    end
+    return if piece_coordinates.nil?
+
+    move_coordinates = [piece_coordinates[0], piece_coordinates[1] + direction]
+    move_coordinates unless fatal_en_passant?(last_piece, move_coordinates)
+  end
+
+  def fatal_en_passant?(last_piece, move_coordinates)
+    @board.set_at(last_piece.position, nil)
+    is_fatal = fatal_move?(move_coordinates)
+    @board.set_at(last_piece.position, last_piece)
+    is_fatal
   end
 end
--- spec/chesspiece_spec/pawn_spec.rb
+++ spec/chesspiece_spec/pawn_spec.rb
@@ -1,6 +1,11 @@
 require "./lib/chesspiece/pawn.rb"
 
 describe Pawn do
+  before(:each) do
+    @board = Board.new
+    @board.set_default
+  end
+
   describe "#initialize" do
     it "stores correct symbol" do
       expect(Pawn.new(:black, nil, nil).instance_variable_get(:@symbol)).to eq("♟")
@@ -9,11 +14,6 @@ describe Pawn do
   end
 
   describe "#valid_moves" do
-    before(:each) do
-      @board = Board.new
-      @board.set_default
-    end
-
     context "white piece" do
       it "returns valid moves in initial position" do
         expect(@board[2, 1].valid_moves).to contain_exactly([2, 2], [2, 3])
@@ -66,6 +66,56 @@ describe Pawn do
         @board[7, 4] = Elephant.new(:white, @board, [7, 4])
         expect(@board[5, 6].valid_moves).to eq([])
       end
+    end
+  end
+
+  describe "#fatal_en_passant?" do
+    it "returns false if the move isn't fatal" do
+      @board.move_piece([4, 1], [4, 3])
+      @board.move_piece([5, 6], [5, 3])
+      expect(@board[5, 3].fatal_en_passant?(@board[4, 3], [4, 2])).to eq(false)
+    end
+
+    it "returns true if the move is fatal" do
+      @board.move_piece([4, 1], [4, 4])
+      @board.move_piece([5, 6], [5, 4])
+      @board.move_piece([4, 0], [6, 2])
+      @board.move_piece([3, 7], [3, 5])
+      expect(@board[4, 4].fatal_en_passant?(@board[5, 4], [5, 5])).to eq(true)
+    end
+
+    it "doesn't affect the board" do
+      @board[4, 1].fatal_en_passant?(@board[0, 6], [4, 3])
+      @board[2, 6].fatal_en_passant?(@board[7, 1], [2, 5])
+      expect(@board.to_s).to eq("8 \e[0;39;49m♜ \e[0m\e[0;39;107m♞ \e[0m\e[0;39;49m▲ \e[0m\e[0;39;107m♛ \e[0m\e[0;39;49m♚ \e[0m\e[0;39;107m▲ \e[0m\e[0;39;49m♞ \e[0m\e[0;39;107m♜ \e[0m\n7 \e[0;39;107m♟ \e[0m\e[0;39;49m♟ \e[0m\e[0;39;107m♟ \e[0m\e[0;39;49m♟ \e[0m\e[0;39;107m♟ \e[0m\e[0;39;49m♟ \e[0m\e[0;39;107m♟ \e[0m\e[0;39;49m♟ \e[0m\n6 \e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\n5 \e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\n4 \e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\n3 \e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\e[0;39;107m  \e[0m\e[0;39;49m  \e[0m\n2 \e[0;39;49m♙ \e[0m\e[0;39;107m♙ \e[0m\e[0;39;49m♙ \e[0m\e[0;39;107m♙ \e[0m\e[0;39;49m♙ \e[0m\e[0;39;107m♙ \e[0m\e[0;39;49m♙ \e[0m\e[0;39;107m♙ \e[0m\n1 \e[0;39;107m♖ \e[0m\e[0;39;49m♘ \e[0m\e[0;39;107m△ \e[0m\e[0;39;49m♕ \e[0m\e[0;39;107m♔ \e[0m\e[0;39;49m△ \e[0m\e[0;39;107m♘ \e[0m\e[0;39;49m♖ \e[0m\n  a b c d e f g h")
+    end
+  end
+
+  describe "#en_passant_coordinates" do
+    it "returns nil if the pawn isn't on the middle row" do
+      @board.move_piece([4, 1], [4, 5])
+      @board.move_piece([5, 6], [5, 5])
+      expect(@board[4, 5].en_passant_coordinates(@board[5, 5])).to be_nil
+    end
+
+    it "returns nil if the last piece isn't a pawn" do
+      @board.move_piece([4, 1], [4, 3])
+      @board.move_piece([5, 7], [5, 4])
+      expect(@board[4, 3].en_passant_coordinates(@board[5, 4])).to be_nil
+    end
+
+    it "returns nil if the last piece moved twice" do
+      @board.move_piece([4, 6], [4, 3])
+      @board.move_piece([5, 1], [5, 3])
+      @board[5, 3].moves = 2
+      expect(@board[4, 3].en_passant_coordinates(@board[5, 3])).to be_nil
+    end
+
+    it "returns move coordinates if the conditions are met" do
+      @board.move_piece([4, 6], [4, 3])
+      @board.move_piece([5, 1], [5, 3])
+      @board[5, 3].moves = 1
+      expect(@board[4, 3].en_passant_coordinates(@board[5, 3])).to eq([5, 2])
     end
   end
 end
